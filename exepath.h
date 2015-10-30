826b70d69391280b33dce5e3397558e9c45c9a96
Tim Myers <tim@jvsw.com>
2015-10-30 15:29:34 -0400

    export symbols to include in dll.

--- exepath.h
+++ exepath.h
@@ -1,3 +1,6 @@
+#include "exepath_export.h"
+
 #include <string>
 
+EXEPATH_EXPORT
 std::string exepath( void *main=0 );
---
+++ exepath_export.h
@@ -0,0 +1,22 @@
+#ifndef exepath_export_h
+#define exepath_export_h
+
+#ifdef WIN32
+#  ifdef EXEPATH_STATIC_DEFINE
+#    define EXEPATH_EXPORT
+#  else
+#    ifndef EXEPATH_EXPORT
+#      ifdef BUILDING_EXEPATH
+         /* We are building this library */
+#        define EXEPATH_EXPORT __declspec(dllexport)
+#      else
+         /* We are using this library */
+#        define EXEPATH_EXPORT __declspec(dllimport)
+#      endif
+#    endif
+#  endif
+#else
+#  define EXEPATH_EXPORT
+#endif
+
+#endif
