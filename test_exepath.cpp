7de2aab5a0a1855e2598a65c8736f517fc6828c8
Tim Myers <tim@jvsw.com>
2015-10-30 15:30:11 -0400

    CMake uses forward slashes.  GetFullPathName flips them.

--- test_exepath.cpp
+++ test_exepath.cpp
@@ -6,12 +6,32 @@ int f() {
   return 0;
 }
 
+std::string library_filepath() {
+#if _WIN32
+  char buf[1024];
+  GetFullPathName(LIBRARY_FILEPATH, 1024, buf, 0);
+  return buf;
+#else
+  return LIBRARY_FILEPATH;
+#endif
+}
+
+std::string thisexe_filepath() {
+#if _WIN32
+  char buf[1024];
+  GetFullPathName(TEST_FILEPATH, 1024, buf, 0);
+  return buf;
+#else
+  return TEST_FILEPATH;
+#endif
+}
+
 TEST_CASE( "Find the object file exepath() is in" ) {
 //  REQUIRE(exepath() == "... /libexepath.dylib");
-  REQUIRE(exepath() == LIBRARY_FILEPATH);
+  REQUIRE(exepath() == library_filepath());
 }
 
 TEST_CASE( "Find the object file f() is in" ) {
 //  REQUIRE(exepath((void*)f) == "... /./test_exepath");
-  REQUIRE(exepath((void*)f) == TEST_FILEPATH);
+  REQUIRE(exepath((void*)f) == thisexe_filepath());
 }
