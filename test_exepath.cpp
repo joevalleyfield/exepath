c034479c00f10b2ed5f108fdcd33e145bb13a52a
Tim Myers <tim@jvsw.com>
2015-10-31 09:03:40 -0400

    dry library_filepath and thisexe_filepath to normalize_filepath.

--- test_exepath.cpp
+++ test_exepath.cpp
@@ -6,32 +6,22 @@ int f() {
   return 0;
 }
 
-std::string library_filepath() {
+const std::string normalize_path(const std::string path) {
 #if _WIN32
   char buf[1024];
-  GetFullPathName(LIBRARY_FILEPATH, 1024, buf, 0);
+  GetFullPathName(path.c_str(), 1024, buf, 0);
   return buf;
 #else
-  return LIBRARY_FILEPATH;
-#endif
-}
-
-std::string thisexe_filepath() {
-#if _WIN32
-  char buf[1024];
-  GetFullPathName(TEST_FILEPATH, 1024, buf, 0);
-  return buf;
-#else
-  return TEST_FILEPATH;
+  return path;
 #endif
 }
 
 TEST_CASE( "Find the object file exepath() is in" ) {
 //  REQUIRE(exepath() == "... /libexepath.dylib");
-  REQUIRE(exepath() == library_filepath());
+  REQUIRE(exepath() == normalize_path(LIBRARY_FILEPATH));
 }
 
 TEST_CASE( "Find the object file f() is in" ) {
 //  REQUIRE(exepath((void*)f) == "... /./test_exepath");
-  REQUIRE(exepath((void*)f) == thisexe_filepath());
+  REQUIRE(exepath((void*)f) == normalize_path(TEST_FILEPATH));
 }
